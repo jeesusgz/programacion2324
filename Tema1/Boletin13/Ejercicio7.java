fdb2aca98699c158ef07b1ca8873650732e87433
Jesus <jesus16400p@gmail.com>
2023-10-11 10:31:13 +0200

    Boletin 1.3 EJ7

--- Tema1/Boletin13/Ejercicio7.java
+++ Tema1/Boletin13/Ejercicio7.java
@@ -3,9 +3,13 @@ package Tema1.Boletin13;
 import java.util.Scanner;
 
 public class Ejercicio7 {
+
+    static final double RETENCION_SOLTERO_DIVORCIADO = 12;
+    static final double RETENCION_MAYORES_50 = 8.5;
+    static final double RETENCION_VIUDO_CASADO = 11.3;
+    static final double RETENCION_RESTO = 10.5;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
 
         System.out.println("Introduce un estado civil [S-C-V-D]");
         char estado = sc.nextLine().charAt(0);
@@ -14,13 +18,13 @@ public class Ejercicio7 {
         int edad = Integer.parseInt(sc.nextLine());
 
         if (estado == 's' && edad < 35 || estado == 'd' && edad < 35){
-            System.out.println("Su porcentaje de retención es de 12%");
+            System.out.println("Su porcentaje de retención es de " + RETENCION_SOLTERO_DIVORCIADO + " %");
         } else if (edad > 50) {
-            System.out.println("Su porcentaje de retención es de 8.5%");
+            System.out.println("Su porcentaje de retención es de " + RETENCION_MAYORES_50 + " %");
         } else if (estado == 'v' && edad < 35 || estado == 'c' && edad < 35) {
-            System.out.println("Su porcentaje de retención es de 11.3%");
+            System.out.println("Su porcentaje de retención es de "+ RETENCION_VIUDO_CASADO + " %");
         }else {
-            System.out.println("Su porcentaje de retención es de 10.5%");
+            System.out.println("Su porcentaje de retención es de " + RETENCION_RESTO +" %");
         }
     }
 }
