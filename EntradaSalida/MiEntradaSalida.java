d2eaadea07f41aeb2bd4471e4d1e7eb169399d90
Jesus <jesus16400p@gmail.com>
2023-10-11 12:44:30 +0200

    Entrada Salida

--- EntradaSalida/MiEntradaSalida.java
+++ EntradaSalida/MiEntradaSalida.java
@@ -5,18 +5,51 @@ import java.util.Scanner;
 public class MiEntradaSalida {
     public static Scanner sc = new Scanner(System.in);
 
+    public static int leerEntero(String mensaje){
+        System.out.printf(mensaje + ": ");
+
+        //TODO Controlar excepción
+        return Integer.parseInt(sc.nextLine());
+    }
+
+    /**
+     * Lee un entero y comprueba si es positivo
+     * @param mensaje lee el mensaje que ha insertado el usuario
+     * @param positivo valor positivo
+     * @return devuelve un valor positivo
+     */
+    public static int LeerEnteroPositivo(String mensaje, int positivo){
+        if (positivo < 0){
+            //TODO Controlar excepcion
+        }
+
+        System.out.printf(mensaje + ": ");
+
+        int numeroLeido;
+
+        do {
+            numeroLeido = Integer.parseInt(sc.nextLine());
+
+            if (numeroLeido< 0){
+                System.out.println("Introduce un número positivo");
+            }
+        } while (numeroLeido < 0);
+
+        return numeroLeido;
+    }
+
     /**
      * Lee un entero comprendido entre el mínimo y el máximo (incluidos)
      * @param min Valor mínimo aceptado
      * @param max Valor máximo aceptado
-     * @return
+     * @return Devuelve un valor entre el mínimo y máximo
      */
     public static int leerEnteroDeRango(String mensaje, int min, int max){
         if (min >= max){
             // TODO: Hacer cuando conozcamos las excepciones
         }
 
-        System.out.printf(mensaje);
+        System.out.printf(mensaje + ": ");
 
         int numeroLeido;
         do {
@@ -28,5 +61,21 @@ public class MiEntradaSalida {
         } while (numeroLeido > min || numeroLeido < max);
 
         return numeroLeido;
+    }
+
+    public static char leerSN(String mensaje){
+        System.out.printf(mensaje + ": ");
+
+        char sn;
+
+        do {
+            sn = sc.nextLine().toUpperCase().charAt(0);
+        }while (sn != 'S' && sn != 'N');
+
+        return sn;
+    }
+
+    public static void cerrarScanner(){
+        sc.close();
     }
 }
