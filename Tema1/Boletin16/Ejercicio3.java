2943f6edc14f3b5a77e1225f36851ee2d55ad30b
Jesus <jesus16400p@gmail.com>
2023-10-16 16:33:57 +0200

    Boletin 1.6 ejercicio 3

---
+++ Tema1/Boletin16/Ejercicio3.java
@@ -0,0 +1,17 @@
+package Tema1.Boletin16;
+
+import java.util.Scanner;
+
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num;
+        do {
+            System.out.println("Introduce un número entero");
+            num = Integer.parseInt(sc.nextLine());
+        }while (num < 0);
+
+        System.out.println("El número " + num + " tiene " + Integer.toString(num).length() + " dígitos");
+    }
+}
