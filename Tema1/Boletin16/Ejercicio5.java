6bb58653e6b5e94d45f5c526f6d50dcca8f93c0b
Jesus <jesus16400p@gmail.com>
2023-10-17 13:56:27 +0200

    Boletin 1.6 ejercicio 5

---
+++ Tema1/Boletin16/Ejercicio5.java
@@ -0,0 +1,28 @@
+package Tema1.Boletin16;
+
+import java.util.Scanner;
+
+public class Ejercicio5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introduce un número");
+        int num = Integer.parseInt(sc.nextLine());
+
+        boolean noEsPrimo = false;
+
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                noEsPrimo = true;
+                break;
+            }
+        }
+
+        if (noEsPrimo) {
+            System.out.println("El número " + num + " NO es primo");
+        }else {
+            System.out.println("El número " + num + " ES primo");
+        }
+    }
+
+}
