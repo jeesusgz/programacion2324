cf45b1d954fa634d9b8d22dd8558eb2045377a2d
Jesus <jesus16400p@gmail.com>
2023-10-25 12:30:29 +0200

    Boletin de Arrays 1

---
+++ Tema2/BoletinArray1/Ejercicio5.java
@@ -0,0 +1,32 @@
+package Tema2.BoletinArray1;
+
+import EntradaSalida.MiEntradaSalida;
+
+import java.util.Arrays;
+
+public class Ejercicio5 {
+
+    private static final int MAX_RANDOM =  1000;
+    public static void main(String[] args) {
+        int tamArray = MiEntradaSalida.LeerEnteroPositivo("Introduce el tamaño del array");
+
+        int[] arrayAleatorio = generaArrayAleatorio(tamArray);
+
+        for (int elem : arrayAleatorio) {
+            System.out.println(elem);
+        }
+
+        //Expresión lambda
+        //Arrays.stream(arrayAleatorio).sorted().forEach(System.out::println);
+    }
+
+    public static int[] generaArrayAleatorio(int tamArray){
+        int[] miArray = new int[tamArray];
+
+        for (int i = 0; i < miArray.length; i++) {
+            miArray[i] = (int) (Math.random() * (MAX_RANDOM + 1));
+        }
+
+        return miArray;
+    }
+}
