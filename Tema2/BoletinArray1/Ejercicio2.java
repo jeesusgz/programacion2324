dec0d534211e92a3bb9876d1490bb5ea94c1f716
Jesus <jesus16400p@gmail.com>
2023-10-25 09:44:02 +0200

    Boletin de Arrays 1 Ejercicio2

--- Tema2/BoletinArray1/Ejercicio2.java
+++ Tema2/BoletinArray1/Ejercicio2.java
@@ -2,6 +2,15 @@ package Tema2.BoletinArray1;
 
 public class Ejercicio2 {
     public static void main(String[] args) {
+        int[] miArray = {2,3,45,-3,-2,433,45,-4,-6,56};
+        int numNegativos = 0;
 
+        for (int i = 0; i < miArray.length; i++) {
+            if (miArray[i] < 0){
+                numNegativos++;
+            }
+        }
+
+        System.out.println("Los números negativos son " + numNegativos);
     }
 }
