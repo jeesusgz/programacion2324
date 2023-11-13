649f52e33885ad98534a6b3b007eb18f2ef2b18d
Jesus <jesus16400p@gmail.com>
2023-11-13 12:45:52 +0100

    Boletin de Arrays 1

--- Tema2/BoletinArray1/Ejercicio11.java
+++ Tema2/BoletinArray1/Ejercicio11.java
@@ -40,4 +40,5 @@ public class Ejercicio11 {
         }
         return pos;
     }
+
 }
---
+++ Tema2/BoletinArray1/OrdenamientoBurbuja.java
@@ -0,0 +1,35 @@
+package Tema2.BoletinArray1;
+
+import java.util.Arrays;
+
+public class OrdenamientoBurbuja {
+    private static final int MAX_RANDOM = 10;
+    public static void main(String[] args) {
+        int[] array = generaArrayAleatorio(MAX_RANDOM);
+        System.out.println(Arrays.toString(ordBurbuja(array)));
+    }
+
+    public static int[] ordBurbuja(int[] a){
+        int aux = 0;
+        for (int i = 1; i < a.length; i++) {
+            for (int j = 0; j < a.length - i; j++) {
+                if (a[j] > a[j + 1]){
+                    aux = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = aux;
+                }
+            }
+        }
+        return a;
+    }
+
+    public static int[] generaArrayAleatorio(int tamArray){
+        int[] miArray = new int[tamArray];
+
+        for (int i = 0; i < miArray.length; i++) {
+            miArray[i] = (int) (Math.random() * (MAX_RANDOM + 1));
+        }
+
+        return miArray;
+    }
+}
