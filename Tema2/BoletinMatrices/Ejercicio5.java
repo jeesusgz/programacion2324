49c2ca9a79de2385442cc2bf7b489168bb1016cc
Jesus <jesus16400p@gmail.com>
2023-11-20 12:08:36 +0100

    Boletin de Array 1

--- Tema2/BoletinMatrices/Ejercicio5.java
+++ Tema2/BoletinMatrices/Ejercicio5.java
@@ -4,10 +4,10 @@ public class Ejercicio5 {
     public static void main(String[] args) {
         int[][] m = {{1,3,5}, {0,2,7}};
 
-        leerMatrizPorFila(m);
+        leerMatrizPorColumnas(m);
     }
 
-    public static void leerMatrizPorFila(int[][] a){
+    public static void leerMatrizPorColumnas(int[][] a){
         System.out.println(" ---> La salida es: ");
         for (int j = 0; j < a[0].length; j++) {
             for (int i = 0; i < a.length; i++) {
