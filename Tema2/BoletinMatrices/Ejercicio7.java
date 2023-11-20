74a3b37319485f3df830be4cb0f778e684a20fc9
Jesus <jesus16400p@gmail.com>
2023-11-20 13:41:57 +0100

    Boletin de Matrices

--- Tema2/BoletinMatrices/Ejercicio7.java
+++ Tema2/BoletinMatrices/Ejercicio7.java
@@ -3,10 +3,10 @@ package Tema2.BoletinMatrices;
 public class Ejercicio7 {
     public static void main(String[] args) {
         int[][] m = {{1,3,5},
-                {0,2,5}
+                    {0,2,7}
         };
         int[][] m2 = {{1,3,5},
-                {0,2,7}
+                    {0,2,7}
         };
 
         if (comprobarMatrizIgual(m,m2)){
@@ -18,13 +18,11 @@ public class Ejercicio7 {
     }
 
     public static boolean comprobarMatrizIgual(int[][] m, int[][] m2){
-        boolean esIgual = false;
+        boolean esIgual= true;
 
-        for (int i = 0; i < m.length; i++) {
-            for (int j = 0; j < m[i].length; j++) {
-                if (m[i][j] == m2[i][j]){
-                    esIgual = true;
-                }else {
+        for (int i = 0; i < m.length && i < m2.length; i++) {
+            for (int j = 0; j < m[i].length && j < m2[i].length; j++) {
+                if (m[i][j] != m2[i][j]){
                     esIgual = false;
                 }
             }
