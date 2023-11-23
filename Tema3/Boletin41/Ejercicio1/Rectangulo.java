1b0764d5c19384f91f3ec892d7fd9c914c5e0ee6
Jesus <jesus16400p@gmail.com>
2023-11-23 14:43:32 +0100

    Boletin 3.1 de objetos

---
+++ Tema3/Boletin41/Ejercicio1/Rectangulo.java
@@ -0,0 +1,27 @@
+package Tema3.Boletin41.Ejercicio1;
+
+public class Rectangulo {
+    private double longitud;
+    private double ancho;
+
+    public Rectangulo(){
+        this.longitud = 1;
+        this.ancho = 1;
+    }
+
+    public double getLongitud() {
+        return longitud;
+    }
+
+    public double getAncho() {
+        return ancho;
+    }
+
+    public void setLongitud(double longitud) {
+        this.longitud = longitud;
+    }
+
+    public void setAncho(double ancho) {
+        this.ancho = ancho;
+    }
+}
