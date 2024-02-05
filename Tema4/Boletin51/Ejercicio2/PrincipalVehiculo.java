8d81d8a5447d06fdc86bbbbad3607f5727c67b56
Jesus <jesus16400p@gmail.com>
2024-02-05 10:27:21 +0100

    Tema 4 Boletin 4.1 Ejercicio 2

--- Tema4/Boletin51/Ejercicio2/PrincipalVehiculo.java
+++ Tema4/Boletin51/Ejercicio2/PrincipalVehiculo.java
@@ -5,66 +5,69 @@ import Tema4.EntradaSalida.Lectora;
 import java.util.Scanner;
 
 public class PrincipalVehiculo {
-    private static final int MAX_VEHICULOS =  200;
+    private static final int MAX_VEHICULOS = 200;
     private static final Vehiculo[] listaVehiculos = new Vehiculo[MAX_VEHICULOS];
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int opcion = 0;
-
+        Scanner teclado = new Scanner(System.in);
+        int opcion;
         do {
             imprimirMenu();
-            opcion = sc.nextInt();
-
+            opcion = teclado.nextInt();
             switch (opcion){
+
                 case 1:
-                    darAltaVehiculo();
+                    darAltaVehiculos();
                     break;
                 case 2:
-
+                    calcularPrecio();
                     break;
                 case 3:
-
+                    imprimirLista();
                     break;
                 case 4:
-                    System.out.println("duro mi negro");
-                    break;
-                default:
-                    System.out.println("Selecciona una opción válida");
+                    System.out.println("Adios");
                     break;
             }
-
-        }while(opcion != 3);
-
+        }while (opcion != 3);
     }
-
-    private static void imprimirMenu(){
-        System.out.println("Escoge una opción");
-        System.out.println("1. Alta de Vehículo");
-        System.out.println("2. Calculo de precio de alquiler");
-        System.out.println("3. Listar todos los vehículos");
-        System.out.println("4. Salir");
+    public static void imprimirMenu(){
+        System.out.println("Elige una opción:");
+        System.out.println("1. Alta vehiculo. ");
+        System.out.println("2. Cálculo del precio del alquiler. ");
+        System.out.println("3. Imprimir lista de vehículos. ");
+        System.out.println("4. Salir. ");
     }
-
-    private static void darAltaVehiculo(){
+    private static void darAltaVehiculos(){
         int seleccionUsuario;
-
-        System.out.println("Estos son los vehículos disponibles");
-
-        int i = 1;
-        for(TipoVehiculo t : TipoVehiculo.values()){
-            System.out.println(++i + ". " + String.valueOf(t).toLowerCase());
+        System.out.println("estos son los vehiculos disponibles");
+        int opcion = 1;
+        for (TipoVehiculo tipoVehiculo : TipoVehiculo.values()){
+            System.out.println(tipoVehiculo);
+            System.out.println(opcion+ ". " +String.valueOf(tipoVehiculo).toLowerCase());
         }
-
-        seleccionUsuario = Lectora.leerEnteroDeRango("Selección: ",TipoVehiculo.values().length,1);
-
-        switch (seleccionUsuario){
-            case 1:
+        seleccionUsuario = Lectora.leerEnteroDeRango("Seleccion: ", TipoVehiculo.values().length, 1);
+    }
+    private static void calcularPrecio(){
+        int posVehiculo;
+        int vehiculosAlta = 0;
+        String matricula =Lectora.solicitarCadenaMayus("Introduce la matricula del vehiculo: ");
+        int dias = Lectora.leerEnteroDeRango("introduzca el numero de dias alquilado: ", 365, 1);
+        for (posVehiculo = 0; posVehiculo < vehiculosAlta; posVehiculo++){
+            if (listaVehiculos[posVehiculo].getMatricula().equals(matricula)){
                 break;
-            case 2:
-                break;
-            case 3:
-                break;
+            }
+        }
+        if (posVehiculo != vehiculosAlta){
+            System.out.println("el vehiculo seleccionado con matricula: "+ matricula + " y tras ser usado por "+dias+" " +
+                    "dias, esto con lleva un precio total de "+listaVehiculos[posVehiculo].getPrecioAlquiler(dias));
+        }else {
+            System.out.println("Su matricula no ha sido detectada ");
+        }
+    }
+    private static void imprimirLista(){
+        for (int i = 0; i < listaVehiculos.length; i++){
+            System.out.println(i);
         }
     }
 }
