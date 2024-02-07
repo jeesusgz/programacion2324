982066cc968eea34e358d736a126adef506dbab5
Jesus <jesus16400p@gmail.com>
2024-02-07 10:32:35 +0100

    Tema 4 Boletin 4.2 Ejercicio 1

--- Tema4/Boletin52/Ejercicio1/EsportTournament.java
+++ Tema4/Boletin52/Ejercicio1/EsportTournament.java
@@ -1,9 +1,17 @@
 package Tema4.Boletin52.Ejercicio1;
 
 public class EsportTournament {
-    public void startTournament(Gamer[] players){
-        for (Gamer p : players){
+    public static void main(String[] args) {
+            FPSGamer fpsPlayer = new FPSGamer();
+            MOBAGamer mobaPlayer = new MOBAGamer();
+            SportGamer sportsPlayer = new SportGamer();
+            Gamer[] players = {fpsPlayer, mobaPlayer, sportsPlayer};
+            startTournament(players);
+    }
 
+    public static void startTournament(Gamer[] players){
+        for (Gamer p : players){
+            p.playGame();
         }
     }
 }
