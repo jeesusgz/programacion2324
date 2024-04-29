63b00c43249782f3dd1865870bc31e8e31613032
Jesus <jesus16400p@gmail.com>
2024-04-29 12:42:01 +0200

    Ejercicio 3 Boletin 6.3

--- Tema6/Boletin72/Ejercicio3/Ejercicio3.java
+++ Tema6/Boletin72/Ejercicio3/Ejercicio3.java
@@ -8,6 +8,10 @@ import org.xml.sax.SAXException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.File;
 import java.io.IOException;
 
 public class Ejercicio3 {
@@ -51,11 +55,74 @@ public class Ejercicio3 {
                 }
             }
             System.out.println();
+
+            for (int i = 0; i < platosBaratos.getLength(); i++) {
+                Element plato = (Element) platosBaratos.item(i);
+                plato.setAttribute("id", String.valueOf(i + 1));
+            }
+
+            Element newElement = doc.createElement("food");
+            newElement.setAttribute("id", String.valueOf(platosBaratos.getLength() + 1));
+            Element newNombre = doc.createElement("name");
+            newNombre.setTextContent("Churros buermu");
+            newElement.appendChild(newNombre);
+            Element newPrecio = doc.createElement("price");
+            newPrecio.setTextContent("1.50€");
+            newElement.appendChild(newPrecio);
+            Element newCalorias = doc.createElement("calories");
+            newCalorias.setTextContent("600");
+            newElement.appendChild(newCalorias);
+            Element newDescripcion = doc.createElement("description");
+            newPrecio.setTextContent("Son unos churros increibles");
+            newElement.appendChild(newDescripcion);
+
+            doc.getDocumentElement().appendChild(newElement);
+
+            File f = new File("./src/Tema6/Boletin72/Ejercicio3/desayuno.xml");
+            Transformer transformer = TransformerFactory.newInstance().newTransformer();
+            transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+
+            StreamResult result = new StreamResult(f);
+            DOMSource source = new DOMSource(doc);
+
+            transformer.transform(source, result);
+
+            for (int i = 0; i < platosBaratos.getLength(); i++) {
+                Element platoMaxCalorias = (Element) platosBaratos.item(i);
+                NodeList listaCalorias = platoMaxCalorias.getElementsByTagName("calories");
+
+                if (listaCalorias.getLength() > 0){
+                    String textoCalorias = listaCalorias.item(0).getTextContent();
+
+                    if (!textoCalorias.isEmpty()){
+                        int calorias = Integer.parseInt(textoCalorias);
+
+                        if (calorias > 500){
+                            platoMaxCalorias.getParentNode().removeChild(platoMaxCalorias);
+                        }
+                    }
+                }
+            }
+
+            File file = new File("./src/Tema6/Boletin72/Ejercicio3/desayuno.xml");
+
+            Transformer transformers = TransformerFactory.newInstance().newTransformer();
+
+            transformers.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+
+            StreamResult results = new StreamResult(file);
+
+            DOMSource sources = new DOMSource(doc);
+            transformer.transform(sources, results);
         } catch (ParserConfigurationException e) {
             throw new RuntimeException(e);
         } catch (IOException e) {
             System.out.println("No se ha podido leer el archivo");
         } catch (SAXException e) {
+            throw new RuntimeException(e);
+        } catch (TransformerConfigurationException e) {
+            throw new RuntimeException(e);
+        } catch (TransformerException e) {
             throw new RuntimeException(e);
         }
     }
